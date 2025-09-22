b5daa7bc795edb7d9f5b5fd0d8071bca9987876a
pjb0811 <pjb0811@gmail.com>
2025-09-23 08:18:19 +0900

    ✨ feat: useScrollToElements 훅 추가
    - 특정 요소로 부드럽게 스크롤하는 기능을 제공하는 useScrollToElements 훅 구현
    - 요소 참조 관리 및 스크롤 위치 조정 기능 포함
    - 옵션을 통해 스크롤 오프셋 설정 가능

---
+++ src/hooks/useScrollToElements/index.ts
@@ -0,0 +1,41 @@
+import { useCallback, useRef } from 'react';
+
+interface Props extends ScrollIntoViewOptions {
+  offset?: number;
+}
+
+export default function useScrollToElements(options?: Props) {
+  const elementRefs = useRef<HTMLElement[]>([]);
+
+  const scrollToElement = useCallback(
+    (index: number) => {
+      if (elementRefs.current[index]) {
+        elementRefs.current[index].scrollIntoView({
+          behavior: 'smooth',
+          block: 'start',
+          inline: 'start',
+          ...options,
+        });
+
+        if (options?.offset) {
+          const top =
+            elementRefs.current[index].getBoundingClientRect().top +
+            window.scrollY -
+            options.offset;
+
+          window.scrollTo({
+            top,
+            behavior: options.behavior || 'smooth',
+          });
+        }
+      }
+    },
+    [options],
+  );
+
+  const setElementRef = useCallback((element: HTMLElement, index: number) => {
+    elementRefs.current[index] = element;
+  }, []);
+
+  return { elementRefs, setElementRef, scrollToElement };
+}
