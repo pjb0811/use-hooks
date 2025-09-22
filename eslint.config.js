3dcf3cd06f09d18efbe32d1d66c2b807e46efbba
pjb0811 <pjb0811@gmail.com>
2025-09-23 07:55:36 +0900

    ✨ feat: 프로젝트 설정 및 구성 파일 업데이트
    - .gitignore 파일 수정: dist 디렉토리 포함
    - .prettierrc 파일 추가: 코드 포맷팅 규칙 설정
    - eslint.config.js 파일 수정: globalIgnores에 .history 추가
    - package.json 및 package-lock.json 파일 업데이트: 의존성 추가 및 스크립트 수정
    - vite.config.ts 파일 수정: 라이브러리 빌드 설정 추가 및 dts 플러그인 통합
    - husky를 이용한 pre-commit 훅 추가: lint-staged 설정

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,12 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import { globalIgnores } from 'eslint/config';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', '.history']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
@@ -20,4 +20,4 @@ export default tseslint.config([
       globals: globals.browser,
     },
   },
-])
+]);
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import dts from 'vite-plugin-dts';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: {
+        index: resolve(__dirname, 'src/index.ts'),
+      },
+      name: 'use-hooks',
+      formats: ['es', 'cjs'],
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
+  plugins: [
+    react(),
+    dts({
+      rollupTypes: true,
+      tsconfigPath: './tsconfig.app.json',
+      //
+    }),
+  ],
+});
